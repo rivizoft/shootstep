2f4ff077bc201250394f12379f92915e2e4f175e
Kirill <shirko49@mail.ru>
2019-05-13 22:29:26 +0500

    new

--- shootstep/resourses.Designer.cs
+++ shootstep/resourses.Designer.cs
@@ -22,14 +22,14 @@ namespace shootstep {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class resources {
+    internal class resourses {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal resources() {
+        internal resourses() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace shootstep {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("shootstep.resources", typeof(resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("shootstep.resourses", typeof(resourses).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;

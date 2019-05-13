c27f15c72c277cdd7df28854e5208f53ad301398
TheCarrot <sevligr@gmail.com>
2019-05-13 21:48:05 +0500

    step event v game

--- shootstep/view/GameWindow.cs
+++ shootstep/view/GameWindow.cs
@@ -6,19 +6,36 @@ namespace shootstep.view
     public class GameWindow : System.Windows.Forms.Form
     {
         private Game _game;
+        private PictureBox _canvas;
+        private Size _defaultSize;
         
-        public GameWindow(Game game)
+        public GameWindow(Game game, Size size)
         {
             _game = game;
-            this.MaximumSize = new Size(512,512);
+            _defaultSize = new Size(size.Width, size.Height);
+            this.Init(size);
+        }
+
+        private void Init(Size size)
+        {
+            this.MaximumSize = size;
             this.MinimumSize = this.MaximumSize;
             this.Size = this.MaximumSize;
             
-            var canvas = new PictureBox();
-            canvas.BackColor = Color.Red;
-            canvas.Size = new Size(128,128);
-            this.Controls.Add(canvas);
-            this.Invalidate();
+            _canvas = new PictureBox();
+            _canvas.BackColor = Color.Black;
+            _canvas.Size = this.Size;
+            //this.Resize += (sender, args) => _canvas.Scale();
+            this.Controls.Clear();
+            this.Controls.Add(_canvas);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var graphics = e.Graphics;
+            graphics.Clear(Color.Black);
+            foreach (var o in _game.GetMap())
+                graphics.DrawImage(o.Sprite);
         }
         
     }
